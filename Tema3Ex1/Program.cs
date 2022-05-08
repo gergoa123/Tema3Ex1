554bbdf7cd5c5e22c37bc26829a55547cc2e128c
Gergoa123 <gergo3124@gmail.com>
2022-05-08 13:14:10 +0300

    Add project files.

---
+++ Tema3Ex1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Tema3Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti 3 numere");
+            int numar1 = int.Parse(Console.ReadLine());
+            int numar2 = int.Parse(Console.ReadLine());
+            int numar3 = int.Parse(Console.ReadLine());
+            int aux = 0;
+
+            if (numar1 >= numar2 & numar1 >= numar3)
+            {
+                aux = numar1;
+            }
+            if (numar2 >= numar1 & numar2 >= numar3)
+            {
+                aux = numar2;
+            }
+            if (numar3 >= numar1 & numar3 >= numar2)
+            {
+                aux = numar3;
+            }
+
+
+            Console.WriteLine("Maximul numerelor este " + aux);
+
+
+        }
+
+
+       
+    }
+}
